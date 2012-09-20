9b977dbf030ae81dbb18ac672ff89143d291c938
David Chelimsky <dchelimsky@drwholdings.com>
2012-09-20 15:37:18 -0500

    4 spaces instead of 8

--- jquery.websocket.js
+++ jquery.websocket.js
@@ -8,39 +8,38 @@
  * Copyright (c) 2010 by shootaroo (Shotaro Tsubouchi).
  */
 (function($){
-$.extend({
+    $.extend({
         websocket: function(url, s) {
-                var ws = WebSocket ? new WebSocket( url ) : {
-                        send: function(m){ return false },
-                        close: function(){}
-                };
-
-                var settings = {
-                    open: function(){},
-                    close: function(){},
-                    message: function(){},
-                    options: {},
-                    events: {}
-                };
-                $.extend(settings, $.websocketSettings, s);
-                $(ws)
-                        .bind('open', settings.open)
-                        .bind('close', settings.close)
-                        .bind('message', settings.message)
-                        .bind('message', function(e){
-                                var m = $.evalJSON(e.originalEvent.data);
-                                var h = settings.events[m.type];
-                                if (h) h.call(this, m);
-                        });
-                ws._send = ws.send;
-                ws.send = function(type, data) {
-                        var m = {type: type};
-                        m = $.extend(true, m, $.extend(true, {}, settings.options, m));
-                        if (data) m['data'] = data;
-                        return this._send($.toJSON(m));
-                }
-                $(window).unload(function(){ ws.close(); ws = null });
-                return ws;
+            var ws = WebSocket ? new WebSocket( url ) : {
+                send: function(m){ return false },
+                close: function(){}
+            };
+            var settings = {
+                open: function(){},
+                close: function(){},
+                message: function(){},
+                options: {},
+                events: {}
+            };
+            $.extend(settings, $.websocketSettings, s);
+            $(ws)
+                .bind('open', settings.open)
+                .bind('close', settings.close)
+                .bind('message', settings.message)
+                .bind('message', function(e){
+                    var m = $.evalJSON(e.originalEvent.data);
+                    var h = settings.events[m.type];
+                    if (h) h.call(this, m);
+                });
+            ws._send = ws.send;
+            ws.send = function(type, data) {
+                var m = {type: type};
+                m = $.extend(true, m, $.extend(true, {}, settings.options, m));
+                if (data) m['data'] = data;
+                return this._send($.toJSON(m));
+            }
+            $(window).unload(function(){ ws.close(); ws = null });
+            return ws;
         }
-});
+    });
 })(jQuery);
