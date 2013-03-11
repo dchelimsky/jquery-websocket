1dbac4ed2e4558bf4807fcdc7f3e262b04277453
Brian Moseley <bcm@maz.org>
2013-03-11 05:00:03 -0700

    replace jquery-json with es5 equivalents
    fixes #3

--- jquery.websocket.js
+++ jquery.websocket.js
@@ -28,7 +28,7 @@
                 .bind('close', settings.close)
                 .bind('message', settings.message)
                 .bind('message', function(e) {
-                    var m = $.evalJSON(e.originalEvent.data);
+                    var m = JSON.parse(e.originalEvent.data);
                     var h = settings.events[m.type];
                     if (h) h.call(this, m);
                 });
@@ -37,7 +37,7 @@
                 var m = {type: type};
                 m = $.extend(true, m, $.extend(true, {}, settings.options, m));
                 if (data) m['data'] = data;
-                return this._send($.toJSON(m));
+                return this._send(JSON.stringify(m));
             };
             $(window).unload(function(){ ws.close(); ws = null; });
             return ws;
